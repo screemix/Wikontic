93ecc251e867e66cba7c84c86915fe2c6768126c
Chepurova Alla <43718473+screemix@users.noreply.github.com>
2025-07-09 16:59:49 +0000

    added wikontic logo

--- Demo.py
+++ Demo.py
@@ -8,7 +8,6 @@ logging.basicConfig(stream=sys.stderr)
 logger = logging.getLogger('App')
 logger.setLevel(logging.ERROR)
 
-
 # Ensure the same user_id across all pages
 if "user_id" not in st.session_state:
     st.session_state.user_id = str(uuid.uuid4())
@@ -17,12 +16,21 @@ user_id = st.session_state.user_id
 
 logger.info(f"User ID: {user_id}")
 
-st.set_page_config(
-    page_title="Knowledge Graph Demo",
-    page_icon="🧠",
-)
+# st.set_page_config(
+#     page_title="Knowledge Graph Demo",
+#     page_icon="🧠",
+# )
 
-st.title("🧠 Knowledge Graph Demo")
+# st.title("🧠 Knowledge Graph Demo")
+st.markdown(
+    """
+    <div style="display: flex; align-items: center;">
+        <img src="wikontic.png" alt="Logo" width="50" style="margin-right: 15px;">
+        <h1 style="margin: 0;">Knowledge Graph Demo</h1>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
 
 st.markdown("""
 Welcome to the **KG Demo App**.
